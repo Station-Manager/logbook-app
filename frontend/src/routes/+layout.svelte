a251012d6ee3f6ddd18aab99bd0ceb57642cfc79
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-12-29 08:05:42 +0200

    Add `load` function and integrate `MainNav` in layout components

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import './layout.css';
+	import MainNav from "$lib/ui/MainNav.svelte";
 
 	let { children } = $props();
 </script>
 
 <header>
+	<MainNav/>
 </header>
 <main>
 	{@render children()}
