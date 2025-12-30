047043a639ed9b26f2cf40d5c6e6e17c27652b5f
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-12-30 11:11:25 +0200

    Refactor `ManagerCardContent` layout to enhance logbook display with updated styling and descriptions.

--- frontend/src/lib/ui/manager/cards/ManagerCardContent.svelte
+++ frontend/src/lib/ui/manager/cards/ManagerCardContent.svelte
@@ -11,19 +11,13 @@
 </script>
 
 <div class="flex flex-row gap-x-2 p-8">
-    <div class="flex flex-col w-[320px] border-r border-r-gray-300">
-        <table>
-            <thead>
-                <tr>
-                    <th>Logbooks</th>
-                </tr>
-            </thead>
-            <tbody>
+    <div class="flex flex-col w-[460px] border-r border-r-gray-300 pr-4">
         {#each logbookList as lb (lb.id)}
-            <tr><td>{lb.name}</td></tr>
+        <div class="flex flex-col border border-gray-300 p-4 rounded-md hover:border-gray-400 hover:cursor-pointer">
+            <div class="font-semibold">{lb.name}</div>
+            <div class="text-sm text-gray-400">{lb.description}</div>
+        </div>
         {/each}
-            </tbody>
-        </table>
     </div>
     <div class="flex flex-col w-full">Col 2</div>
 </div>
