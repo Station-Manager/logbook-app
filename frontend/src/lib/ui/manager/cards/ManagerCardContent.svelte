8081c174f0bc929fb08061e2ea3c8db60b02de58
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-12-31 07:39:52 +0200

    Expand `ManagerCardContent` layout to include input fields for logbook name and description, update styling and adjust column widths

--- frontend/src/lib/ui/manager/cards/ManagerCardContent.svelte
+++ frontend/src/lib/ui/manager/cards/ManagerCardContent.svelte
@@ -8,6 +8,7 @@
     let {
         logbookList = [],
     }: Props = $props();
+    let inputDisabled: boolean = $state(true);
 
     const newLogbookAction = (): void => {
         console.log("New logbook action");
@@ -19,7 +20,7 @@
 </script>
 
 <div class="flex flex-row gap-x-2 p-8">
-    <div class="flex flex-col w-[460px] h-[580px] border-r border-r-gray-300 pr-4 gap-y-4">
+    <div class="flex flex-col w-[580px] h-[580px] border-r border-r-gray-300 pr-10 gap-y-4">
         <button onclick={newLogbookAction} class="flex flex-col border border-gray-300 p-4 rounded-md hover:border-gray-400 hover:cursor-pointer">
             <span class="font-semibold">New Log Book</span>
         </button>
@@ -30,7 +31,24 @@
         </button>
         {/each}
     </div>
-    <div class="flex flex-col w-full">
-
+    <div class="flex flex-col w-full pl-10">
+        <div class="w-[360px]">
+            <div class="flex flex-col gap-y-4">
+                <div>
+                    <label for="name" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Name</label>
+                    <div class="mt-2">
+                        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 dark:bg-white/5 dark:outline-white/10 dark:focus-within:outline-indigo-500">
+                            <input disabled={inputDisabled} id="name" type="text" placeholder="Log book name" class="block bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none"/>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <label for="about" class="block text-sm/6 font-medium text-gray-900">Description</label>
+                    <div class="mt-2">
+                        <textarea disabled={inputDisabled} id="about" name="about" rows="3" class="resize-none block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
