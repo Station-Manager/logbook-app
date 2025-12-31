72a6fce6847c6bee7c00d7f68e12d8571b9d98f1
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-12-31 09:06:05 +0200

    Enhance `ManagerCardContent` with full-state logbook management, add input bindings, disable state, and refine layout interactions

--- frontend/src/lib/states/logbook-state.svelte.ts
+++ frontend/src/lib/states/logbook-state.svelte.ts
@@ -1,17 +1,39 @@
 import { types } from '$lib/wailsjs/go/models';
 
 export interface LogbookState {
+    id: number;
+    name: string;
+    callsign: string;
+    description: string | undefined;
+    clear(this: LogbookState): void;
     fromLogbook(this: LogbookState, lb: types.Logbook): void;
     toLogbook(this: LogbookState): types.Logbook;
 }
 
 export const logbookState: LogbookState = $state({
+    id: 0,
+    name: '',
+    callsign: '',
+    description: '',
+    clear(this: LogbookState): void {
+        this.id = 0;
+        this.name = '';
+        this.callsign = '';
+        this.description = '';
+    },
     fromLogbook(this: LogbookState, lb: types.Logbook): void {
+        this.id = lb.id;
+        this.name = lb.name;
+        this.callsign = lb.callsign;
+        this.description = lb.description ?? '';
         console.log(lb);
     },
     toLogbook(this: LogbookState): types.Logbook {
         const base = new types.Logbook();
-        console.log(base);
+        base.id = this.id;
+        base.name = this.name;
+        base.callsign = this.callsign;
+        base.description = this.description;
         return base;
     },
 });
--- frontend/src/lib/ui/manager/cards/ManagerCardContent.svelte
+++ frontend/src/lib/ui/manager/cards/ManagerCardContent.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
     import {types} from "$lib/wailsjs/go/models";
+    import {configState} from "$lib/states/config-state.svelte";
+    import {onMount} from "svelte";
+    import {logbookState} from "$lib/states/logbook-state.svelte";
 
     interface Props {
         logbookList: types.Logbook[];
     }
+
+    let nameInput: HTMLInputElement;
+    let callsignInput: HTMLInputElement;
+    let descriptionInput: HTMLTextAreaElement;
 
     let {
         logbookList = [],
@@ -11,12 +18,18 @@
     let inputDisabled: boolean = $state(true);
 
     const newLogbookAction = (): void => {
-        console.log("New logbook action");
+        logbookState.clear();
+        inputDisabled = false;
+        nameInput.focus();
     }
 
     const loadLogbookAction = (id: number): void => {
         console.log("Load logbook action", id);
     }
+
+    onMount((): void => {
+        logbookState.fromLogbook(configState.logbook);
+    });
 </script>
 
 <div class="flex flex-row gap-x-2 p-8">
@@ -25,7 +38,7 @@
             <span class="font-semibold">New Log Book</span>
         </button>
         {#each logbookList as lb (lb.id)}
-        <button onclick={() => loadLogbookAction(lb.id)} class="flex flex-col border border-gray-300 p-4 rounded-md text-left hover:border-gray-400 hover:cursor-pointer">
+        <button disabled={lb.id === configState.logbook.id} onclick={() => loadLogbookAction(lb.id)} class="flex flex-col border border-gray-300 p-4 rounded-md text-left hover:border-gray-400 hover:cursor-pointer disabled:hover:border-gray-300 disabled:cursor-not-allowed">
             <span class="font-semibold">{lb.name}</span>
             <span class="text-sm text-gray-400">{lb.description}</span>
         </button>
@@ -37,17 +50,55 @@
                 <div>
                     <label for="name" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Name</label>
                     <div class="mt-2">
-                        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 dark:bg-white/5 dark:outline-white/10 dark:focus-within:outline-indigo-500">
-                            <input disabled={inputDisabled} id="name" type="text" placeholder="Log book name" class="block bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none"/>
+                        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                            <input
+                                    bind:this={nameInput}
+                                    bind:value={logbookState.name}
+                                    disabled={inputDisabled}
+                                    id="name"
+                                    type="text"
+                                    placeholder="Log book name"
+                                    class="block bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none"/>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <label for="about" class="block text-sm/6 font-medium text-gray-900">Description</label>
+                    <label for="callsign" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Callsign</label>
                     <div class="mt-2">
-                        <textarea disabled={inputDisabled} id="about" name="about" rows="3" class="resize-none block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"></textarea>
+                        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 dark:bg-white/5 dark:outline-white/10 dark:focus-within:outline-indigo-500">
+                            <input
+                                    bind:this={callsignInput}
+                                    bind:value={logbookState.callsign}
+                                    disabled={inputDisabled}
+                                    id="callsign"
+                                    type="text"
+                                    placeholder="Callsign"
+                                    class="block bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none"/>
+                        </div>
                     </div>
                 </div>
+                <div>
+                    <label for="description" class="block text-sm/6 font-medium text-gray-900">Description</label>
+                    <div class="mt-2">
+                        <textarea
+                                bind:this={descriptionInput}
+                                bind:value={logbookState.description}
+                                disabled={inputDisabled}
+                                id="description"
+                                rows="3"
+                                class="resize-none block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-between mt-4">
+            <div>
+            <button class="rounded-md bg-red-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Delete</button>
+            </div>
+            <div class="flex gap-x-4">
+            <button class="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Select</button>
+            <button class="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+            <button class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50">Cancel</button>
             </div>
         </div>
     </div>
