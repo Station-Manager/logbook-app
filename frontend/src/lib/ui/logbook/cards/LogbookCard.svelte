c77e59d831c19a67f0256cf40d6e94f65317b91e
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-01-16 15:43:38 +0200

    Enhance `LogbookCard.svelte` with shared-utils frequency formatting; adjust column widths for improved layout readability.

--- frontend/src/lib/ui/logbook/cards/LogbookCard.svelte
+++ frontend/src/lib/ui/logbook/cards/LogbookCard.svelte
@@ -3,7 +3,7 @@
     import {onMount} from "svelte";
     import {types} from "$lib/wailsjs/go/models";
     import {GetQsoSlice} from "$lib/wailsjs/go/facade/Service";
-    import {formatDate} from "@station-manager/shared-utils";
+    import {formatCatKHzToDottedMHz, formatDate, parseDatabaseFreqToDottedKhz} from "@station-manager/shared-utils";
 
     let pageNum = 1;
     let tableRows: types.Qso[] = $state<types.Qso[]>([]);
@@ -26,9 +26,10 @@
     <div role="table">
         {#each tableRows as qso, index (qso.id)}
             <div id="row-{index}" role="row" class="flex flex-row">
-                <div>{formatDate(qso.qso_date)}</div>
-                <div>{qso.band}</div>
-                <div>{qso.freq}</div>
+                <div class="w-28">{formatDate(qso.qso_date)}</div>
+                <div class="w-32">{qso.call}</div>
+                <div class="w-14">{qso.band}</div>
+                <div>{parseDatabaseFreqToDottedKhz(qso.freq)}</div>
                 <div>{qso.submode}</div>
                 <div>{qso.notes}</div>
             </div>
