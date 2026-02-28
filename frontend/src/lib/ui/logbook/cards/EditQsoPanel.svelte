52d78f053b80cb1509c503c52c64c9f0a7469bfe
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-02-28 10:38:58 +0200

    Improve `EditQsoPanel` layout: adjust input structure, add uppercase utility class, and refine spacing for better usability.

--- frontend/src/lib/ui/logbook/cards/EditQsoPanel.svelte
+++ frontend/src/lib/ui/logbook/cards/EditQsoPanel.svelte
@@ -3,6 +3,7 @@
     import {onMount} from "svelte";
     import {qsoEditState} from "$lib/states/qso-edit-state.svelte";
     import FormControls from "$lib/ui/logbook/components/FormControls.svelte";
+    import {inputBaseUppercase} from "@station-manager/shared-utils";
 
     interface Props {
         qsoId: number
@@ -26,11 +27,19 @@
 </script>
 
 <div>
-    <div class="h-129">
-        <label for="call">Callsign</label>
-        <input id="call"/>
+    <div class="flex h-126">
+    <div class="h-32 w-full border">
+        <div class="w-37.5">
+            <label for="call">Callsign</label>
+            <input
+                    id="call"
+                    autocomplete="off"
+                    spellcheck="false"
+                    class={inputBaseUppercase}/>
+        </div>
     </div>
-    <div class="flex justify-end">
+    </div>
+    <div class="flex justify-end mt-2">
         <FormControls submitFunc={onSubmit} cancelFunc={onCancel}/>
     </div>
 </div>
