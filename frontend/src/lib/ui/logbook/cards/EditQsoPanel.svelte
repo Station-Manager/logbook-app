d25358688ac52f971095e69b427fb6a03836d015
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-03-13 20:09:53 +0200

    Refine `EditQsoPanel`: add reusable label styles and mode selection dropdown.

--- frontend/src/lib/ui/logbook/cards/EditQsoPanel.svelte
+++ frontend/src/lib/ui/logbook/cards/EditQsoPanel.svelte
@@ -4,7 +4,7 @@
     import FormControls from "$lib/ui/logbook/components/FormControls.svelte";
     import {
         inputBase,
-        inputBaseUppercase,
+        inputBaseUppercase, labelBase, selectBase, selectWrapper,
     } from "@station-manager/shared-utils";
     import {GetQsoById} from "$lib/wailsjs/go/facade/Service";
     import {handleAsyncError} from "$lib/utils/error-handler";
@@ -15,6 +15,7 @@
     }
 
     const focusContext = getFocusContext();
+    const labelCss = labelBase + " mb-2";
 
     let {qsoId}: Props = $props();
     let qso: QsoEditState = $state(qsoEditState);
@@ -41,7 +42,7 @@
     <div class="flex flex-col h-126">
         <div class="flex flex-row gap-x-4 h-20 w-full">
             <div class="w-37.5">
-                <label for="call">Callsign</label>
+                <label for="call" class={labelCss}>Callsign</label>
                 <input
                         bind:value={qso.call}
                         bind:this={focusContext.refs.callsignInput}
@@ -51,7 +52,7 @@
                         class={inputBaseUppercase}/>
             </div>
             <div class="w-37.5">
-                <label for="band">Band</label>
+                <label for="band" class={labelCss}>Band</label>
                 <input
                         id="band"
                     type="text"
@@ -62,7 +63,7 @@
                     disabled/>
             </div>
             <div class="w-37.5">
-                <label for="freq">Frequency</label>
+                <label for="freq" class={labelCss}>Frequency</label>
                 <input
                         id="freq"
                         type="text"
@@ -73,7 +74,7 @@
                         disabled/>
             </div>
             <div class="w-37.5">
-                <label for="freq_rx">Frequency RX</label>
+                <label for="freq_rx" class={labelCss}>Frequency RX</label>
                 <input
                         id="freq_rx"
                         type="text"
@@ -83,10 +84,23 @@
                         spellcheck="false"
                         disabled/>
             </div>
+            <div class="w-37.5">
+                <label for="freq_rx" class={labelCss}>Mode</label>
+                <div class="{selectWrapper}">
+                    <select class={selectBase} bind:value={qso.submode}>
+                        <option value="USB">USB</option>
+                        <option value="LSB">LSB</option>
+                    </select>
+                    <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
+                        <path fill-rule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+
+            </div>
         </div>
         <div class="flex flex-row gap-x-4 h-32 w-full">
             <div class="w-37.5">
-                <label for="date_on">Date On</label>
+                <label for="date_on" class={labelCss}>Date On</label>
                 <input
                         id="date_on"
                         type="date"
@@ -96,7 +110,7 @@
                         spellcheck="false"/>
             </div>
             <div class="w-37.5">
-                <label for="date_off">Date Off</label>
+                <label for="date_off" class={labelCss}>Date Off</label>
                 <input
                         id="date_off"
                         type="date"
@@ -106,7 +120,7 @@
                         spellcheck="false"/>
             </div>
             <div class="w-37.5">
-                <label for="time_on">Time On</label>
+                <label for="time_on" class={labelCss}>Time On</label>
                 <input
                         id="time_on"
                         type="time"
@@ -116,7 +130,7 @@
                         spellcheck="false"/>
             </div>
             <div class="w-37.5">
-                <label for="time_off">Time Off</label>
+                <label for="time_off" class={labelCss}>Time Off</label>
                 <input
                         id="time_off"
                         type="time"
